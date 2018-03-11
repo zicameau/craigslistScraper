0ec96c9615578b5ef9bc709b16f492f00f99c8a4
Trevor <rowdeil@gmail.com>
2018-03-10 22:01:59 -0800

    Search Through Listing Titles and Their Content
    Added the functionality to search through each listing title for a certain regex or search through that listings title for that regex. Need to cut down on the amount that it is searching in the content section in order to speed up the program.

--- craigslistScraper.py
+++ craigslistScraper.py
@@ -1,16 +1,17 @@
 """  craigslistScraper.py -
     Description: Grabs queries from craigslist and stores them for the user.
 """
-import argparse
-
+import argparse, json, os
+from scrapeCraigslist import scrapeCraigslist
 
 
 def parser():
-
+    
     parser = argparse.ArgumentParser(description='Craigslist Scraper Program')
     parser.add_argument('-u','--locationURL', help='URL to your craigslist location. EX: https://sacramento.craigslist.org/', required=True, dest = 'url')
     parser.add_argument('-r','--regex', help='Regular expression that we will search through craigslist for.', required=True, dest = 'regex')
-    parser.add_argument('-c','--categories', help='Command To Execute, Wrap in quotes(\'\') for commands with multiple fields.', required=False, dest = 'cat')
+    parser.add_argument('-c','--categories', help='Categories that you would like to search through.  If not passed will search through all categories.',
+                        required=False, dest = 'cats', default = 'all',nargs = '*')
 
     return parser.parse_args()
 
@@ -20,15 +21,16 @@ if __name__ == "__main__":
 
     args = parser()
 
-    url = args.url
-    print("Url: " + url)
-    regex = args.regex
-    print("Regex: " + regex)
+    print(args)
 
-    if 'cat' in vars(args):
-        cat = args.cat
-        print("Category: " + str(cat))
+    url = args.url
+    regex = args.regex
+
+    if 'cats' in args.__dict__:
+        cats = args.cats
+        results = scrapeCraigslist(url, regex, cats)
+    else:
+        results = scrapeCraigslist(url, regex)
     
     
-    
-    #main()
+
